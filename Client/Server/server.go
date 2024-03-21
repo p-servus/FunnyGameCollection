604051d8ec2353236a679d1dbbe9f9fecba063c0
Sebastian Große <p.servus.servus@gmail.com>
2024-03-21 21:52:22 +0100

    adds server to the client

---
+++ Client/Server/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const schema = "http"
+const host = "localhost"
+const port = 8080
+
+var url = fmt.Sprintf("%s://%s:%d", schema, host, port)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("../Static"))
+	http.Handle("/", fileServer)
+
+	log.Printf("Starting server for client on %s!", url)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}

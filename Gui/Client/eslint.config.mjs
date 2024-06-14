898bdae4b10a5442e3a01d2b0b817cf13a0b7a50
Johannes Große <johannes.christian.grosse@gmail.com>
2024-06-14 19:07:04 +0200

    basic setup eslint for jsdoc, semi and some other rules

--- Gui/Client/eslint.config.mjs
+++ Gui/Client/eslint.config.mjs
@@ -1,13 +1,34 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+import jsdoc from 'eslint-plugin-jsdoc';
 
 
 export default [
+  // {
+  //   languageOptions: {globals: globals.browser },
+  // },
+  // pluginJs.configs.recommended,
   {
-    languageOptions: {globals: globals.browser },
+    plugins: {
+      jsdoc,
+    },
     rules: {
-      "no-unused-vars": "off",
+      "semi": ["error", "never"],
+      "no-unused-vars": "error",
+      "prefer-const": "error",
+      "jsdoc/require-jsdoc": [
+        "error",
+        {
+          "require": {
+              "FunctionDeclaration": true,
+              "MethodDefinition": true,
+              "ClassDeclaration": true,
+              "ArrowFunctionExpression": true,
+              "FunctionExpression": true
+          }
+        }
+      ],
+      'jsdoc/require-description': "error",
     }
-  },
-  pluginJs.configs.recommended,
+  }
 ];

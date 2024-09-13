0b1d98ca92aea25080f69f38c453e619934a5531
Johannes Große <johannes.christian.grosse@gmail.com>
2024-09-13 19:14:49 +0200

    adjust eslint-config

--- Gui/Client/eslint.config.mjs
+++ Gui/Client/eslint.config.mjs
@@ -9,6 +9,11 @@ export default [
       "semi": ["error", "never"],
       "no-unused-vars": "error",
       "prefer-const": "error",
+      
+      'comma-dangle': ['error', 'always-multiline'],
+      'quotes': ['error', 'single'],
+      'no-var': 'error',
+      
       "jsdoc/require-jsdoc": [
         "error",
         {
@@ -21,7 +26,14 @@ export default [
           }
         }
       ],
-      'jsdoc/require-description': "error",
+      'jsdoc/require-description': 'error',
+      'jsdoc/require-param': 'error',
+      'jsdoc/require-param-description': 'error',
+      'jsdoc/require-param-type': 'error',
+      'jsdoc/require-returns': 'error',
+      'jsdoc/require-returns-type': 'error',
+      'jsdoc/require-returns-description': 'error',
+      'jsdoc/require-returns-check': 'error',
     }
   }
 ]

f69f04c0ed1ee39363aa3e55212b90493b80476a
Johannes Große <johannes.christian.grosse@gmail.com>
2024-09-24 21:01:02 +0200

    configure type Element for jsdoc to suppress "jsdoc/no-undefined-types" warning

--- Gui/Client/eslint.config.mjs
+++ Gui/Client/eslint.config.mjs
@@ -34,7 +34,14 @@ export default [
       'jsdoc/require-returns-type': 'error',
       'jsdoc/require-returns-description': 'error',
       'jsdoc/require-returns-check': 'error',
-      'jsdoc/no-undefined-types': 1, // to avoid 'is defined but never used' when importing types only for jsdoc-comments
+      'jsdoc/no-undefined-types': [
+        'warn',
+        {
+          'definedTypes': [
+            'Element',
+          ],
+        },
+      ],
     },
   },
 ]
